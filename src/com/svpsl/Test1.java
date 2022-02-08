27100e2d74d896756c75025a1b960ea8e8699551
akhil <akhil@DESKTOP-6HSIED8>
2022-02-08 09:06:09 +0530

    Third Commit....

---
+++ src/com/svpsl/Test1.java
@@ -0,0 +1,16 @@
+package com.svpsl;
+
+
+public class Test1 {
+
+	
+	public static void main(String[] args) {
+		String str = "india";
+		if(str == "india") {
+			System.out.println("Hello India!!!");
+		}else {
+			System.out.println("Welcome India!!!");
+		}
+	}
+	
+}
