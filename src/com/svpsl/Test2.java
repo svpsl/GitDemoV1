a7bf8ff82502f6e112906a8367d6816d31ca6717
user <user@DESKTOP-P0A10V8>
2022-02-09 09:51:31 -0800

    Third changes commit..........

--- src/com/svpsl/Test2.java
+++ src/com/svpsl/Test2.java
@@ -6,7 +6,7 @@ public class Test2 {
 	
 	public static void main(String[] args) {
 		String str = "india";
-		if(str == "india") {
+		if(str == "india" && str != null) {
 			System.out.println("Hello India!!!");
 		}else {
 			System.out.println("Welcome India!!!");
