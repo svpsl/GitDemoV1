71c6c8c9bcfa276a1d71d2195cd82a3c364a0b54
RajKumar8112020 <63899793+RajKumar8112020@users.noreply.github.com>
2022-02-13 11:58:26 -0800

    Fisrt Commit of Test3

---
+++ src/com/svpsl/Test3.java
@@ -0,0 +1,16 @@
+package com.svpsl;
+
+
+public class Test3 {
+
+	
+	public static void main(String[] args) {
+		String str = "Karnataka";
+		if(str == "Karnataka" && str != null) {
+			System.out.println("Hello Karnataka!!!===========");
+		}else {
+			System.out.println("Welcome Karnataka!!!========");
+		}
+	}
+	
+}
