b099975e419422f111782fd30dcde71b12438883
RAJ KUMAR GUPTA <raj1sjce@gmail.com>
2022-02-13 12:15:14 -0800

    I 2nd Committed of Test3.....

--- src/com/svpsl/Test3.java
+++ src/com/svpsl/Test3.java
@@ -6,10 +6,10 @@ public class Test3 {
 	
 	public static void main(String[] args) {
 		String str = "Karnataka";
-		if(str == "Karnataka" && str != null) {
-			System.out.println("Hello Karnataka!!!===========");
+		if(str == "Karnataka" && str != null && str =="Arv") {
+			System.out.println("Hello   Arv Karnataka!!!===========");
 		}else {
-			System.out.println("Welcome Karnataka!!!========");
+			System.out.println("Welcome Arv Karnataka!!!========");
 		}
 	}
 	
